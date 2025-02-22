382bed0a0b496e1d0fd9ff6b67f2eb470ef25b00
alfredosalgado <alfresal88@gmail.com>
2025-02-22 18:56:31 -0300

    cambiofinal4

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,3 +31,78 @@ document.addEventListener("DOMContentLoaded", () => {
 
   textos.forEach(texto => observer.observe(texto));
 });
+
+
+
+
+
+function animateCounter(counter) {
+  const target = +counter.getAttribute('data-target');
+  const isMoney = counter.classList.contains('money');
+  let count = 0;
+  const increment = target / 100; // Ajusta la velocidad de animación
+
+  const updateCounter = () => {
+    count += increment;
+
+    if (count < target) {
+      counter.textContent = isMoney ? `$ ${Math.floor(count).toLocaleString()}` : Math.floor(count);
+      requestAnimationFrame(updateCounter);
+    } else {
+      counter.textContent = isMoney ? `$ ${target.toLocaleString()}` : target;
+    }
+  };
+
+  updateCounter();
+}
+
+// Observer para detectar cuando los contadores sean visibles
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      const counterBox = entry.target;
+      counterBox.classList.add("visible"); // Aparece suavemente
+
+      const counters = counterBox.querySelectorAll(".counter");
+      counters.forEach(counter => {
+        counter.textContent = "0"; // Reinicia el conteo cada vez que es visible
+        animateCounter(counter);
+      });
+    }
+  });
+}, { threshold: 0.5 }); // Se activa cuando al menos el 50% del elemento es visible
+
+// Aplicar el observer a cada contador
+document.querySelectorAll('.counter-box').forEach(counterBox => observer.observe(counterBox));
+
+
+// main.js
+document.querySelector('.whatsapp-btn').addEventListener('click', function() {
+  const phone = '56920700893'; // Número de teléfono
+  const message = 'Hola, estoy interesado en más información.'; // Mensaje predefinido
+  window.open(`https://wa.me/${phone}?text=${encodeURIComponent(message)}`, '_blank');
+});
+
+
+// Obtén el botón de "Subir"
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+// Cuando el usuario haga scroll hacia abajo, muestra el botón
+window.onscroll = function () {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    scrollToTopBtn.style.display = "flex"; // Muestra el botón
+  } else {
+    scrollToTopBtn.style.display = "none"; // Oculta el botón
+  }
+};
+
+// Cuando el usuario haga clic en el botón, realiza el desplazamiento suave
+scrollToTopBtn.addEventListener("click", function (event) {
+  event.preventDefault(); // Evita la acción predeterminada de ancla
+
+  // Desplazamiento suave hasta la parte superior
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth", // Añade la transición suave
+  });
+});
